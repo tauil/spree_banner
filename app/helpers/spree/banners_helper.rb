ea2be39e9f6a8331f35a7955ec0d279918b73e18
Rafael B. Tauil <rafael@tauil.com.br>
2013-07-31 21:12:36 -0300

    Fixes flash snippet on iE and FF

--- app/helpers/spree/banners_helper.rb
+++ app/helpers/spree/banners_helper.rb
@@ -14,19 +14,11 @@ module Spree
       banner = Banner.enable(category).limit(max)
       if !banner.blank?
         if banner.first.attachment_content_type == 'application/x-shockwave-flash'
-          raw "<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0\" width=\"960\" height=\"270\">
-    <param name=\"movie\" value=\"#{banner.first.attachment.url}\">
-    <param name=\"quality\" value=\"high\">
-    <param name=\"menu\" value=\"false\">
-    <!--[if !IE]> <-->
-    <object data=\"#{banner.first.attachment.url}\" width=\"960\" height=\"270\" type=\"application/x-shockwave-flash\" title=\"Adobe Flash Player\">
-      <param name=\"quality\" value=\"high\">
-      <param name=\"menu\" value=\"false\">
-      <param name=\"pluginurl\" value=\"http://www.macromedia.com/go/getflashplayer\">
-      FALHA (o navegador deveria mostrar algum conteúdo em Flash ao invés disto).
-    </object>
-    <!----> <!--[endif]---->
-  </object>"
+          if request.headers["HTTP_USER_AGENT"].include?("MSIE")
+            raw "<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0\" width=\"960\" height=\"270\"><param name=\"movie\" value=\"#{banner.first.attachment.url}\"><param name=\"quality\" value=\"high\"><param name=\"menu\" value=\"false\"></object>"
+            else
+            raw "<object data=\"#{banner.first.attachment.url}\" width=\"960\" height=\"270\" type=\"application/x-shockwave-flash\" title=\"Adobe Flash Player\"><param name=\"quality\" value=\"high\"><param name=\"menu\" value=\"false\"><param name=\"pluginurl\" value=\"http://www.macromedia.com/go/getflashplayer\"></object>"
+          end
         else
           banner = banner.sort_by { |ban| ban.position }
           if (style == "list")
