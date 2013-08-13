68bc0a36253ac495f7ce3923a3737160637b71f7
Rafael B. Tauil <rafael@tauil.com.br>
2013-08-13 00:16:22 -0300

    Fixes requests without headers errors

--- app/helpers/spree/banners_helper.rb
+++ app/helpers/spree/banners_helper.rb
@@ -14,7 +14,7 @@ module Spree
       banner = Banner.enable(category).limit(max)
       if !banner.blank?
         if banner.first.attachment_content_type == 'application/x-shockwave-flash'
-          if request.headers["HTTP_USER_AGENT"].include?("MSIE")
+          if request.headers["HTTP_USER_AGENT"].present? && request.headers["HTTP_USER_AGENT"].include?("MSIE")
             raw "<object classid=\"clsid:D27CDB6E-AE6D-11cf-96B8-444553540000\" codebase=\"http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0\" width=\"960\" height=\"270\"><param name=\"movie\" value=\"#{banner.first.attachment.url}\"><param name=\"quality\" value=\"high\"><param name=\"menu\" value=\"false\"></object>"
             else
             raw "<object data=\"#{banner.first.attachment.url}\" width=\"960\" height=\"270\" type=\"application/x-shockwave-flash\" title=\"Adobe Flash Player\"><param name=\"quality\" value=\"high\"><param name=\"menu\" value=\"false\"><param name=\"pluginurl\" value=\"http://www.macromedia.com/go/getflashplayer\"></object>"
